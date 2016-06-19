59655ac264a02b1c70030d5121f657c1a1157ddb
javflores <jflores@findmypast.com>
2016-06-19 16:27:29 +0100

    FoodPoisoning now has a main app with basic elm architecture functions.

--- food_poisoning/web/elm/FoodPoisoning.elm
+++ food_poisoning/web/elm/FoodPoisoning.elm
@@ -1,6 +1,33 @@
 module FoodPoisoning exposing (..)
 
-import Html
+import Html.App as Html
+import Html exposing (..)
 
-main =
-    Html.text "Hello from Elm"
+type alias Model =
+    String
+
+type Message = NoOp
+
+init : ( Model, Cmd Message )
+init =
+    ("Woo hoo hoo, I'm in a view", Cmd.none)
+
+update : Message -> Model -> ( Model, Cmd Message )
+update message model =
+    (model, Cmd.none)
+
+subscriptions : Model -> Sub Message
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Message
+view model =
+    text model
+
+main = 
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
